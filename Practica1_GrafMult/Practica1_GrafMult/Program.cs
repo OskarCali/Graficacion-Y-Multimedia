5e231ccfdf9d67319120b5d104ae56d98b270f93
Oskar <joscali98@micorreo.upp.edu.mx>
2018-09-24 18:33:12 -0500

    Practica No. 1 - Hello World

---
+++ Practica1_GrafMult/Practica1_GrafMult/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1_GrafMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey();
+        }
+    }
+}
